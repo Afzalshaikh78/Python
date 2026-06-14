e37ce056a0f99f7dcb2f6d33494ae60425be01a1
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2026-06-14 14:04:54 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,49 @@
+# name = "afzal"
+# print(name)
+
+# age = 20
+# print(age)
+
+
+# a = 10
+
+# b = 10.4
+
+# s = "Hello World"
+
+# print(a)
+# print(b)
+# print(s)
+# x = a+b;
+# print("The sum of a and b is: ", x)
+
+# is_true = False
+# print(is_true)
+
+
+my_list = [1,2,3,4,5]
+print(my_list[0])
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+print(fruits[1])
+
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple[0])
+print(my_tuple[1])
+print(len(my_tuple))
+
+dictionary = {
+  "name":"afzal",
+  "age":20,
+}
+
+
+a = 10
+b  = 20
+
+if(a>b):
+  print("a is greater than b")
+else:
+    print("b is greater than a")
